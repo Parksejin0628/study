762760caefbd489a0a251150dfa025d37931e793
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2021-12-20 11:51:51 +0900

    temp

---
+++ Beakjoon Online Judge question solving/[C]boj_11066(21.12.20)/[C]boj_11066(21.12.20).cpp
@@ -0,0 +1,16 @@
+//number of question : 11066
+//Difficulty : Gold 3
+//Time to solve : 
+//Simple review : 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+
+int main(void)
+{
+	
+	
+	return 0;
+}
