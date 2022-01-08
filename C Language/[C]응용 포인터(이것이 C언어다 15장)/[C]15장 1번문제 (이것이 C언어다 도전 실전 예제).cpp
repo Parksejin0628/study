b732e4697d35aee32762179a6475f722d03c3c4d
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2022-01-08 20:24:43 +0900

    study C language_application pointer(3)
    Total studying time : 6582m (+50m) (109.7h / 4.57d)

--- C Language/[C]응용 포인터(이것이 C언어다 15장)/[C]15장 1번문제 (이것이 C언어다 도전 실전 예제).cpp
+++ C Language/[C]응용 포인터(이것이 C언어다 15장)/[C]15장 1번문제 (이것이 C언어다 도전 실전 예제).cpp
@@ -1,48 +1,74 @@
 #include<stdio.h>
+#include<string.h>
+
+int input_name(char (*name)[20]);
+void print_name(char (*name)[20], int cnt);
+int check_overlap_name(char *input, char (*name)[20], int cnt);
 
 int main(void)
 {
-	int i = 0;
-	int j = 0;
-	int num[5][6] = {0};
-	int now = 0;
+	char name[10][20];
+	int cnt;
 	
-	for(i=0; i<5; i++)
-	{
-		for(j=0; j<6; j++)
-		{
-			if(i==4)
-			{
-				num[i][j] = num[0][j] + num[1][j] + num[2][j] + num[3][j];
-				continue;
-			}
-			if(j==5)
-			{
-				num[i][j] = num[i][0] + num[i][1] + num[i][2] + num[i][3] + num[i][4];
-				break;
-			}
-			now++;
-			num[i][j] = now;
-		}
-	} 
-	
-	for(i=0; i<5; i++)
-	{
-		for(j=0; j<6; j++)
-		{
-			printf("%5d", num[i][j]);
-		}
-		printf("\n");
-	}
+	cnt = input_name(name);
+	print_name(name, cnt);
 	
 	scanf("%d");
 	
 	return 0;
 }
 
+int input_name(char (*name)[20])
+{
+	char input[20] = {'\0'};
+	int cnt = 0;
+	
+	while(1)
+	{
+		printf("이름 : ");
+		gets(input);
+		if(strcmp(input, "end")==0 || cnt > 9)
+		{
+			printf("# 총 %d명이 입력되었습니다.\n", cnt);
+			break;
+		}
+		if(check_overlap_name(input, name, cnt) == 0)
+		{
+			strcpy(name[cnt], input);
+			cnt++;
+		}
+		else
+		{
+			printf("# 이름이 이미 등록되었습니다!\n");
+		}
+	}
+	
+	return cnt;
+}
+int check_overlap_name(char *input, char (*name)[20], int cnt)
+{
+	int i = 0;
+	for(i=0; i<cnt; i++)
+	{
+		if(strcmp(input, name[i]) == 0)	return 1;
+	}
+	return 0;
+}
+
+void print_name(char (*name)[20], int cnt)
+{
+	int i = 0;
+	for(i=0; i<cnt; i++)
+	{
+		printf("%s\n", name[i]);
+	}
+	
+	return;
+}
+
 
 /*
-도전 1 가로 세로의 합 구하기  
- - 5행 6열의 2차원 배열을 선언하고 4행 5열 부분은 1부터 20까지 초기화합니다. 
-   초기화된 배열에서 마지막 열의 요소에는 각 행의 합을 저장하고 마지막 행의 요소에는 각 열의 합을 저장한 후 전체 배열의 값을 출력합니다. 
+도전 1 방명록 프로그램
+ - 키보드로 이름을 반복 입력한 후 입력된 모든 이름을 출력합니다. 이름은 최대 10명까지 입력할 수 있으며 같은 이름이 입력되면 중복 메시지를 출력하고 end가 입력되면 입력을 끝냅니다.
+   이름을 저장할 2차원 char 배열은 main 함수에 작성하고 이름을 입력하는 함수, 중복 여부를 검사하는 함수, 출력하는 함수를 작성하여 완성합니다.  
 */ 
--- C Language/[C]응용 포인터(이것이 C언어다 15장)/[C]15장 2번문제 (이것이 C언어다 도전 실전 예제).cpp
+++ C Language/[C]응용 포인터(이것이 C언어다 15장)/[C]15장 2번문제 (이것이 C언어다 도전 실전 예제).cpp
@@ -1,52 +1,11 @@
 #include<stdio.h>
 #include<string.h>
 
+void swap(char *type, int *pa, int *pb);
+
 int main(void)
 {
-	char word[15][25];
-	char research[25];
-	int index = 0;
-	int i = 0; 
-	int check = 0;
 	
-	while(1)
-	{
-		printf("단어 입력 (종료는 end 입력) : ");
-		gets(word[index]);
-		printf("check\n");
-		if(strcmp(word[index], "end") == 0)
-		{
-			printf("# 총 %d개의 단어가 입력되었습니다!\n\n", index);
-			break;
-		}
-		index++;
-	}
-	
-	while(1)
-	{
-		printf("검색 단어 (종료는 end 입력) : ");
-		gets(research);
-		if(strcmp(research, "end") == 0)
-		{
-			break;
-		}
-		for(i=0; i<index; i++)
-		{
-			if(strcmp(word[i], research) == 0)
-			{
-				printf("# %d번째 같은 단어가 있습니다!\n", i+1);
-				check = 1;
-				break;
-			}
-		}
-		if(check == 0)
-		{
-			printf("# 없는 단어입니다.\n");
-		}
-		check = 0;
-	}
-	
-	 
 	
 	scanf("%d");
 	
@@ -55,6 +14,6 @@ int main(void)
 
 
 /*
-도전 2 단어 검색 프로그램   
- - 키보드로 여러 개의 단어를 입력한 후에 원하는 단어를 찾는 프로그램을 작성합니다. 입력 단어의 수는 최대 10개까지며 한 단어의 길이는 최대 20자로 제한합니다. 
+도전 2 프로필 교환 프로그램
+ - 두 명의 나이와 키를 입력한 후에 바꾸어 출력합니다. 단, 나이와 키를 바꾸는 함수는 int형과 double형을 모두 교환할 수 있도록 하나의 함수로 구현합니다.
 */ 
