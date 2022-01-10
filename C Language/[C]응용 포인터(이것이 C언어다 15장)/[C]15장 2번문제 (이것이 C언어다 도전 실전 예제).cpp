e994d16225b879149c2d51fd748c55ead489f44e
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2022-01-10 20:31:43 +0900

    study C language_application pointer(end)
    Total studying time : 6638m (+56m) (110.6h / 4.60d)

--- C Language/[C]응용 포인터(이것이 C언어다 15장)/[C]15장 2번문제 (이것이 C언어다 도전 실전 예제).cpp
+++ C Language/[C]응용 포인터(이것이 C언어다 15장)/[C]15장 2번문제 (이것이 C언어다 도전 실전 예제).cpp
@@ -1,15 +1,50 @@
 #include<stdio.h>
 #include<string.h>
 
-void swap(char *type, int *pa, int *pb);
+void swap(char *type, void *pa, void *pb);
 
 int main(void)
 {
+	int age1;
+	int age2;
+	double height1;
+	double height2;
 	
+	printf("첫 번째 사람의 나이와 키 입력 : ");
+	scanf("%d %lf", &age1, &height1);
+	printf("두 번째 사람의 나이와 키 입력 : ");
+	scanf("%d %lf", &age2, &height2);
+	
+	swap("int", (int*)&age1, (int*)&age2);
+	swap("double", (double*)&height1, (double*)&height2);
+	
+	printf("첫 번째 사람의 나이와 키 : %d, %.1lf\n", age1, height1);
+	printf("두 번재 사람의 나이와 키 : %d, %.1lf\n", age2, height2);
 	
 	scanf("%d");
 	
 	return 0;
+}
+
+void swap(char *type, void *pa, void *pb)
+{
+	if(strcmp(type, "int") == 0)
+	{
+		int temp;
+		temp = *(int*)pa;
+		*(int*)pa = *(int*)pb;
+		*(int*)pb = temp;
+	}
+	else
+	{
+		double temp;
+		temp = *(double*)pa;
+		*(double*)pa = *(double*)pb;
+		*(double*)pb = temp;
+	}
+
+	
+	return;
 }
 
 
--- C Language/[C]응용 포인터(이것이 C언어다 15장)/[C]15장 3번문제 (이것이 C언어다 도전 실전 예제).cpp
+++ C Language/[C]응용 포인터(이것이 C언어다 15장)/[C]15장 3번문제 (이것이 C언어다 도전 실전 예제).cpp
@@ -1,77 +1,103 @@
 #include<stdio.h>
+#include<string.h>
 
+void init_intro(char (*intro)[80], char **pint);
+int input_intro(char (*intro)[80]);
+void sort_intro(char **pint, int cnt);
+void print_intro(int mode, void *vp, int cnt); 
 
 int main(void)
 {
-	int branch[4][7] = { {70, 45, 100, 92, 150, 81}, {88, 92, 77, 30, 52, 55}, {50, 90, 88, 75, 77, 49}, {120, 92, 80, 150, 130, 105} };
-	int *rank[4] = {branch[0], branch[1], branch[2], branch[3]};
-	int *temp;
-	int i = 0;
-	int j = 0;
+	char intro[10][80];
+	char *pint[10];
+	int cnt;
 	
-	for(i=0; i<7; i++)
-	{
-		for(j=0; j<4; j++)
-		{
-			if(i==6)
-			{
-				branch[j][6] = (int)(branch[j][6] / 6.0 + 0.5);
-				continue;
-			}
-			branch[j][6] += branch[j][i];
-		}
-	}
-	for(i=0; i<4; i++)
-	{
-		for(j=i+1; j<4; j++)
-		{
-			if(rank[i][6] < rank[j][6])
-			{
-				temp = rank[i];
-				rank[i] = rank[j];
-				rank[j] = temp;
-			}
-		}
-	}
-	
-	printf("실적별 출력...\n");
-	for(i=0; i<4; i++)
-	{
-		if(rank[i] == branch[0])	printf("관악점");
-		else if(rank[i] == branch[1])	printf("강남점");
-		else if(rank[i] == branch[2])	printf("명동점");
-		else if(rank[i] == branch[3])	printf("대림점"); 
-		for(j=0; j<7; j++)
-		{
-			printf("%5d", rank[i][j]);
-		}
-		printf("\n");
-	}
-	
-	printf("\n지점별 출력...\n");
-	for(i=0; i<4; i++)
-	{
-		if(i==0)	printf("관악점");
-		else if(i==1)	printf("강남점");
-		else if(i==2)	printf("명동점");
-		else if(i==3)	printf("대림점"); 
-		for(j=0; j<7; j++)
-		{
-			printf("%5d", branch[i][j]);
-		}
-		printf("\n");
-	}
-	
-	
+	init_intro(intro, pint);
+	cnt = input_intro(intro);
+	sort_intro(pint, cnt);
+	print_intro(1, pint, cnt);
+	print_intro(0, intro, cnt);
 	
 	scanf("%d");
 	
 	return 0;
 }
 
+void init_intro(char (*intro)[80], char **pint)
+{
+	int i = 0;
+	for(i=0; i<10; i++)
+	{
+		pint[i] = intro[i];
+	}
+	
+	return;
+}
+
+int input_intro(char (*intro)[80])
+{
+	int cnt = 0;
+	char input[80];
+	
+	while(1)
+	{
+		printf("인사말 입력 : ");
+		gets(input);
+		if(strcmp(input, "end") == 0 || cnt >= 10)
+		{
+			return cnt;
+		}
+		strcpy(intro[cnt++], input);
+	}
+	
+	return cnt;
+}
+
+void sort_intro(char **pint, int cnt)
+{
+	int i = 0;
+	int j = 0;
+	char *temp;
+	for(i=0; i<cnt; i++)
+	{
+		for(j=i+1; j<cnt; j++)
+		{
+			if(strcmp(pint[i], pint[j]) > 0)
+			{
+				temp = pint[i];
+				pint[i] = pint[j];
+				pint[j] = temp;
+			}
+		}
+	}
+}
+
+void print_intro(int mode, void *vp, int cnt)
+{
+	int i = 0;
+	if(mode==1)
+	{
+		printf("# 사전순으로 출력...\n");
+		for(i=0; i<cnt; i++)
+		{
+			printf("%s\n", *((char**)vp + i));
+		}
+	}
+	else	
+	{
+		printf("# 입력순으로 출력...\n");
+		for(i=0; i<cnt; i++)
+		{
+			printf("%s\n", (char*)vp + (80*i));
+		}
+	}
+	
+	return;
+}
+
 
 /*
-도전 3 지점별 실적 관리 프로그램  
- - 또또 분식의 지점별 상반기 매출을 순위별로 정렬하여 출력합니다. 각 지점별 상반기 평균 매출을 구한 후에 매출이 가장 좋은 지점부터 차례로 출력합니다.
-   이어서 원래의 매출 현황을 한 번 더 출력합니다. 지점별 상반기 매출은 다음과 같이 초기화하며 평균 매출의 소수점 이하 값은 반올림합니다. 
+도전 3 인사말 정렬 프로그램
+ - 키보드로 인사말을 입력한 후에 사전순으로 정렬하여 출력합니다. 이어서 입력한 순서대로 다시 한 번 출력합니다.
+   정렬은 입력한 문자열을 바꾸지 않고 포이터 배열의 연결 순서를 바꾸어 구현합니다. 
 */ 
