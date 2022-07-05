90b0c6ca4054c6c3f639a65d52bc01666a939c1b
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2022-07-05 20:10:24 +0900

    boj 2178 question solving(end)
    Total studying time : 9324m (+49m) (155.4h / 6.47d)

--- Beakjoon Online Judge question solving/[C]boj_2178(21.06.29~07.01, 22.07.02~)/[C++]boj_2178(2)(22.07.02).cpp
+++ Beakjoon Online Judge question solving/[C]boj_2178(21.06.29~07.01, 22.07.02~)/[C++]boj_2178(2)(22.07.02).cpp
@@ -2,14 +2,16 @@
 
 #include<iostream>
 
+void printMaze();
+
 using std::cin;
-using std::cout;·á 
+using std::cout;
 using std::endl;
 
 typedef struct Pos
 {
-	int x;
-	int y;
+	int row;
+	int column;
 }pos;
 
 class Maze
@@ -19,7 +21,6 @@ private:
 	int** maze_val;
 	pos* searchPos;
 	char** maze_oriVal;
-	int searchPos_frontIndex;
 	int maze_row;
 	int maze_column;
 public:
@@ -28,22 +29,23 @@ public:
 	bool input_mazeOriVal();
 	bool BFS();
 	bool printAnswer();
+	void printMaze();
 };
 
-Maze::Maze(int n, int m) : move_count(-1), searchPos_frontIndex(0), maze_row(n), maze_column(m)
+Maze::Maze(int n, int m) : move_count(0), maze_row(n), maze_column(m)
 {
-	maze_val = new int*[maze_row+2];
+	maze_val = new int*[maze_row+2]{};
 	for(int i=0; i<=maze_row+1; i++)
 	{
-		maze_val[i] = new int[maze_column+2];
+		maze_val[i] = new int[maze_column+2]{};
 	}
 	
-	searchPos = new pos[maze_row*maze_column];
+	searchPos = new pos[maze_row*maze_column +1]{};
 	
-	maze_oriVal = new char*[maze_row];
+	maze_oriVal = new char*[maze_row]{};
 	for(int i=0; i<maze_row; i++)
 	{
-		maze_oriVal[i] = new char[maze_column+2];
+		maze_oriVal[i] = new char[maze_column+2]{};
 	}
 }
 Maze::~Maze()
@@ -72,22 +74,97 @@ bool Maze::input_mazeOriVal()
 		{
 			//cout<<(int)maze_oriVal[i][j]<<" ";
 			maze_val[i+1][j+1] = (int)(maze_oriVal[i][j] - '0');
-			cout<<maze_val[i+1][j+1]<<" ";
-			
+		}
+	}
+	searchPos[0] = {1, 1};
+	maze_val[1][1] = 2;
+	/*for(int i=0; i<=maze_row+1; i++)
+	{
+		for(int j=0; j<=maze_column+1; j++)
+		{
+			cout<<maze_val[i][j]<<" ";
 		}
 		cout<<endl;
-	}
+	}*/
 	
+	return true;
 }
 
 bool Maze::printAnswer()
 {
+	if(BFS()==false)
+	{
+		cout<<"BFS ¿¡·¯"<<endl;
+	}
+	cout<<move_count; 
 	
+	return true;
 }
 
 bool Maze::BFS()
 {
+	int row;
+	int column;
+	int frontIndex = 0;
+	int rearIndex = 0;
+	int nextIndex = 1;
+	while(frontIndex < maze_row * maze_column)
+	{
+		move_count++;
+		//cout<<"ÇöÀç ÀÌµ¿Ä­°³¼ö : "<<move_count<<endl;
+		//printMaze();
+		while(frontIndex <= rearIndex)
+		{
+			row = searchPos[frontIndex].row;
+			column = searchPos[frontIndex].column;
+			//cout<<"ÇöÀç Å½»ö ÁÂÇ¥ : "<<row<<" "<<column<<endl;
+			if(row == maze_row && column == maze_column)
+			{
+				return true;
+			}
+			if(maze_val[row-1][column] == 1)
+			{
+				searchPos[nextIndex] = {row-1, column};
+				nextIndex++;
+				maze_val[row-1][column] = 2;
+			}
+			if(maze_val[row+1][column] == 1)
+			{
+				searchPos[nextIndex] = {row+1, column};
+				nextIndex++;
+				maze_val[row+1][column] = 2;
+			}
+			if(maze_val[row][column-1] == 1)
+			{
+				searchPos[nextIndex] = {row, column-1};
+				nextIndex++;
+				maze_val[row][column-1] = 2;
+			}
+			if(maze_val[row][column+1] == 1)
+			{
+				searchPos[nextIndex] = {row, column+1};
+				nextIndex++;
+				maze_val[row][column+1] = 2;
+			}
+			frontIndex++;
+		}
+		rearIndex = nextIndex - 1;
+	}
 	
+	return false;
+}
+
+void Maze::printMaze()
+{
+	for(int i=0; i<=maze_row+1; i++)
+	{
+		for(int j=0; j<=maze_column+1; j++)
+		{
+			cout<<maze_val[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	cout<<endl;
 }
 
 int main(void)
