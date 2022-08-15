fdadcdfe0836decb12fcd00ab5022622c0166f9b
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2022-08-15 14:13:47 +0900

    boj_7662 (3)
    Total studying time : 10460m (+82m) (174.3h / 7.26d)

--- Beakjoon Online Judge question solving/[C++]boj_7662(2)(22.08.13)/[C++]boj_7662(2)(22.08.13, 14)/[C++]boj_7662(2)(22.08.13, 14)/main.cpp
+++ Beakjoon Online Judge question solving/[C++]boj_7662(2)(22.08.13)/[C++]boj_7662(2)(22.08.13, 14)/[C++]boj_7662(2)(22.08.13, 14)/main.cpp
@@ -3,6 +3,7 @@
 using std::cout;
 using std::cin;
 using std::endl;
+// pop에서 값 교환할 때 자식 둘 중 더 큰 자식과 교환하는 과정 해야된다.
 
 class Data
 {
@@ -35,6 +36,10 @@ public:
 	{
 		return num;
 	}
+	void printData()
+	{
+		cout << num << "(" << deleteCheck << ")";
+	}
 };
 
 class DoublePriorityQueue
@@ -48,17 +53,22 @@ private:
 	int rearIndex_minQueue;
 	int parentIndex;
 	int childIndex;
+	bool dataEmpty;
 public:
-	DoublePriorityQueue() : rearIndex_maxQueue(1), rearIndex_minQueue(1), parentIndex(0), childIndex(0) { }
+	DoublePriorityQueue() : rearIndex_maxQueue(1), rearIndex_minQueue(1), parentIndex(0), childIndex(0)
+	, newData(NULL), temp_swap(NULL), maxQueue{}, minQueue{}, dataEmpty(true)
+	{ }
 	void inputNewData(int input)
 	{
 		newData = new Data(input);
+		dataEmpty = false;
 		//maxQueue
 		childIndex = rearIndex_maxQueue;
 		parentIndex = childIndex / 2;
 		maxQueue[rearIndex_maxQueue++] = newData;
 		while (parentIndex > 0)
 		{
+			//testcode
 			if (maxQueue[parentIndex]->getNum() < maxQueue[childIndex]->getNum())
 			{
 				temp_swap = maxQueue[parentIndex];
@@ -91,22 +101,169 @@ public:
 				break;
 			}
 		}
+		cout << "데이터 입력 후 " << endl;
+		printArr();
 
 		return;
 	}
 	int maxPop(bool del)
 	{
+		if (dataEmpty)
+		{
+			return -1;
+		}
 		Data* popData = maxQueue[1];
 		int retNum = popData->getNum();
 		
-		maxQueue[1] = maxQueue[rearIndex_maxQueue--];
-		parentIndex = 1;
-		childIndex = parentIndex * 2;
-		while(childIndex < rearIndex_maxQueue)
+		while(rearIndex_maxQueue>1)
+		{
+			popData = maxQueue[1];
+			retNum = popData->getNum();
+			maxQueue[1] = maxQueue[--rearIndex_maxQueue];
+			if (rearIndex_maxQueue <= 1 && del == true)
+			{
+				dataEmpty = true;
+			}
+			parentIndex = 1;
+			childIndex = parentIndex * 2;
+			while (childIndex < rearIndex_maxQueue)
+			{
+				if (maxQueue[parentIndex]->getNum() < maxQueue[childIndex]->getNum())
+				{
+					temp_swap = maxQueue[parentIndex];
+					maxQueue[parentIndex] = maxQueue[childIndex];
+					maxQueue[childIndex] = temp_swap;
+					parentIndex = childIndex;
+					childIndex = parentIndex;
+				}
+				else if (childIndex + 1 < rearIndex_maxQueue && maxQueue[parentIndex]->getNum() < maxQueue[childIndex + 1]->getNum())
+				{
+					childIndex++;
+					temp_swap = maxQueue[parentIndex];
+					maxQueue[parentIndex] = maxQueue[childIndex];
+					maxQueue[childIndex] = temp_swap;
+					parentIndex = childIndex;
+					childIndex = parentIndex;
+				}
+				else
+				{
+					break;
+				}
+			}
+			
+			if (popData->deleteData(del))
+			{
+				cout << "maxPop실행 후(한번 더)" << endl;
+				printArr();
+				delete popData;
+				popData = NULL;
+				continue;
+			}
+			cout << "maxPop실행 후(최종)" << endl;
+			printArr();
+			break;
+		}
+		if (popData == NULL && del == false)
+		{
+			dataEmpty = true;
+		}
+		return retNum;
 	}
-	int minPop(bool del);
-	void init();
-	void printAnswer();
+
+	int minPop(bool del)
+	{
+		if (dataEmpty)
+		{
+			return -1;
+		}
+		Data* popData = minQueue[1];
+		int retNum = popData->getNum();
+
+		while(rearIndex_minQueue > 1)
+		{
+			popData = minQueue[1];
+			retNum = popData->getNum();
+			minQueue[1] = minQueue[--rearIndex_minQueue];
+			if (rearIndex_minQueue <= 1 && del == true )
+			{
+				dataEmpty = true;
+			}
+			parentIndex = 1;
+			childIndex = parentIndex * 2;
+			while (childIndex < rearIndex_minQueue)
+			{
+				if (minQueue[parentIndex]->getNum() > minQueue[childIndex]->getNum())
+				{
+					temp_swap = minQueue[parentIndex];
+					minQueue[parentIndex] = minQueue[childIndex];
+					minQueue[childIndex] = temp_swap;
+					parentIndex = childIndex;
+					childIndex = parentIndex;
+				}
+				else if (childIndex + 1 < rearIndex_minQueue && minQueue[parentIndex]->getNum() > minQueue[childIndex + 1]->getNum())
+				{
+					childIndex++;
+					temp_swap = minQueue[parentIndex];
+					minQueue[parentIndex] = minQueue[childIndex];
+					minQueue[childIndex] = temp_swap;
+					parentIndex = childIndex;
+					childIndex = parentIndex;
+				}
+				else
+				{
+					break;
+				}
+			}
+			if (popData->deleteData(del))
+			{
+				delete popData;
+				popData = NULL;
+				cout << "minPop실행 후(한번 더)" << endl;
+				printArr();
+				continue;
+			}
+			cout << "minPop실행 후(최종)" << endl;
+			printArr();
+			break;
+		}
+		if (popData == NULL && del == false)
+		{
+			dataEmpty = true;
+		}
+		return retNum;
+	}
+	void printAnswer()
+	{
+		int max = maxPop(false);
+		int min = minPop(false);
+
+		if (dataEmpty)
+		{
+			cout << "EMPTY" << endl;
+		}
+		else
+		{
+			cout << max << " " << min << endl;
+		}
+
+		return;
+	}
+	void printArr()
+	{
+		cout << "maxQueue" << endl;
+		for (int i = 1; i < rearIndex_maxQueue; i++)
+		{
+			maxQueue[i]->printData();
+			cout << " ";
+		}
+		cout << endl << "minQueue" << endl;
+		for (int i = 1; i < rearIndex_minQueue; i++)
+		{
+			minQueue[i]->printData();
+			cout << " ";
+		}
+		cout << endl << endl;
+	}
 };
 
 int main(void)
@@ -115,7 +272,7 @@ int main(void)
 	int commandcase = 0;
 	char command = '\0';
 	int inputNum = 0;
-	DoublePriorityQueue doublePriorityQueue;
+	DoublePriorityQueue* doublePriorityQueue = new DoublePriorityQueue;
 
 	cin >> testcase;
 	for (int t = 1; t <= testcase; t++)
@@ -126,17 +283,17 @@ int main(void)
 			cin >> command >> inputNum;
 			if (command == 'I')
 			{
-				doublePriorityQueue.inputNewData(inputNum);
+				doublePriorityQueue->inputNewData(inputNum);
 			}
 			else if (command == 'D')
 			{
 				if (inputNum == 1)
 				{
-					doublePriorityQueue.maxPop(true);
+					doublePriorityQueue->maxPop(true);
 				}
 				else if (inputNum == -1)
 				{
-					doublePriorityQueue.minPop(true);
+					doublePriorityQueue->minPop(true);
 				}
 				else
 				{
@@ -150,8 +307,9 @@ int main(void)
 				return 0;
 			}
 		}
-		doublePriorityQueue.printAnswer();
-		doublePriorityQueue.init();
+		doublePriorityQueue->printAnswer();
+		delete doublePriorityQueue;
+		doublePriorityQueue = new DoublePriorityQueue;
 	}
 
 	return 0;
