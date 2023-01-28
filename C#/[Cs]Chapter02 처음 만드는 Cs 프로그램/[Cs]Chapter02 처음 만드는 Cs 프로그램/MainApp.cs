98529efcc4832e23c91a793c72af148f28bbd1e1
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2023-01-28 12:02:28 +0900

    [C#]Chapter02 처음 만드는 Cs 프로그램

---
+++ C#/[Cs]Chapter02 처음 만드는 Cs 프로그램/[Cs]Chapter02 처음 만드는 Cs 프로그램/MainApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using static System.Console;
+
+namespace Simple
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?");
+            Console.WriteLine("반갑습니다!");
+        }
+    }
+}
