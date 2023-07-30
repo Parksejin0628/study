b9e4e403f856ecb56b057372360b0fdf161d009d
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2023-07-30 11:36:28 +0900

    [Cs]Chapter06 학습(end)
    Total studying time : 12596m (+39m) (209.9h / 8.74d)

--- C#/[Cs]Chapter06 메소드로 코드 간추리기/[Cs]Chapter06 메소드로 코드 간추리기/Program.cs
+++ C#/[Cs]Chapter06 메소드로 코드 간추리기/[Cs]Chapter06 메소드로 코드 간추리기/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Transactions;
 
 namespace Chapter6
 {
     class mainApp
     {
+        //6.4 참조에 의한 매개변수 전달
         static public void Swap_PassByValue(int x, int y)
         {
             int temp = x;
@@ -28,27 +30,64 @@ namespace Chapter6
             Console.WriteLine("a : " + a);
             Console.WriteLine("b : " + b);
         }
-
+        //6.5 메소드의 결과를 참조로 반환하기
         static public ref int add2(ref int x)
         {
             x = x + 2;
             return ref x;
         }
+        //6.6. 출력 전용 매개변수
+        static private void DoubleUp(int a, int b, out int c, out int d)
+        {
+            c = a * 2;
+            d = b * 2;
+        }
+        //6.8. 가변 개수의 인수
+        static private int Sum(params int[] x)
+        {
+            int sum = 0;
+            for(int i=0; i<x.Length; i++)
+            {
+                sum += x[i];
+            }
+
+            return sum;
+        }
+
+        static double Square(double arg)
+        {
+            return arg * arg;
+        }
+
 
         static void Main(string[] args)
         {
             int a = 3;
             int b = 7;
-
+            //6.4 참조에 의한 매개변수 전달
             Swap_PassByValue(a, b);
             PrintAB(a, b);
             Swap_PassByReference(ref a, ref b);
             PrintAB(a, b);
-
+            //6.5 메소드의 결과를 참조로 반환하기
             ref int c = ref add2(ref a);
             PrintAB(a, b);
             PrintAB(c, b);
-            
+            //6.6. 출력 전용 매개변수
+            DoubleUp(a, b, out int i, out int j);
+            PrintAB(i, j);
+
+            //6.8. 가변 개수의 인수
+            int d = Sum(1, 2, 5, 7, 8, 11);
+            Console.WriteLine(d);
+
+            //연습문제 1
+            Console.Write("수를 입력하세요 : ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+            Console.WriteLine("결과 : {0}", Square(arg));
+
         }
     }
 }
@@ -120,9 +159,36 @@ namespace Chapter6
     1) 메소드 오버로딩 : 하나의 메소드 이름에 여러 개의 구현을 하는 것
     2) 매개변수의 수와 형식에 따라 다른 버전의 메소드를 호출해준다.
         - 반환 형식은 구별할 방법이 없으므로 구별하지 않는다.
+        ex) public static int func(int a, int b) / public static int func(int a) / public static int func(double a, double b) 모두 메소드 오버라이딩 가능
     3) 유연한 메소드 제공이 가능하여 생산성을 높여준다.
 
+<6.8. 가변 개수의 인수>
+1. 가변 개수의 인수 : 개수가 유연하게 변할 수 있는 인수
+    1) 사용법 : 매개변수 앞에 params를 붙이고 매개변수를 배열로 선언한다.
+        ex) int Sum(params int[] args) { ... }
+    2) 활용법 : 인수의 개수만 유연하게 달라질 수 있는 함수에 사용하기 좋다.
+2. 다른 매개변수와 이용할 때는 가변 개수의 인수가 무조건 마지막에 있어야 한다.
 
+<6.9. 명명된 인수>
+1. 명명된 인수
+    1) 정의 : 메소드를 호출할 때 순서가 아닌 인수의 이름으로 데이터를 할당할 수 있는 기능
+    2) 사용법 : '인수명 : 전달할 데이터 값'
+        ex) PrintProfile(name : "박세진", phone : "010-2708-3709");
+    3) 순서에 의존할 필요가 없으며 코드의 가독성을 높여준다.
 
+<6.10. 선택적 인수>
+1. 선택적 인수
+    1) 정의 : 매개변수에 기본 값을 선언하여 인수를 선택적으로 받는 것
+    2) 사용법 : 함수 선언에서 매개변수에 초기값을 대입하면 된다.
+        ex) public static int fuc(int a, int b = 3);
+    3) 주의점 : 선택적 인수는 항상 필수 인수 뒤에 와야 한다. 즉, 기본 값을 가진 매개변수가 없는 매개변수 앞에 있어선 안된다.
+    4) 활용 : 매개변수에 기본 값을 선언한 경우 그 인수는 전달하지 않아도 기본값으로 메소드가 실행된다.
+    5) 코드 유지보수 단계에서 헷갈릴 가능성이 있으므로 명명된 인수와 함께 사용하면 유용하다.
+
+<6.11. 로컬 함수>
+1. 로컬 함수
+    1) 정의 : 메소드 안에서 선언되고 메소드 안에서만 사용 가능한 함수이다.
+    2) 특징 : 자신이 존재하는 지역의 변수를 사용할 수 있다.
+    3) 활용 : 특정 메소드 안에서만 반복적으로 사용 되는 것을 로컬 함수로 정의하여 코드를 간추릴 수 있다.
 
 */
