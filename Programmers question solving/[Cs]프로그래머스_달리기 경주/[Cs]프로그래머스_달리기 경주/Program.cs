aa03d6ebc0fa3fc17f7630922637dd79d899e6a6
Parksejin0628 <64259593+Parksejin0628@users.noreply.github.com>
2023-08-02 14:02:33 +0900

    [Cs] Chapter07_클래스(1)
    Total studying time : 12639m (+43m) (210.6h / 8.77d)

---
+++ Programmers question solving/[Cs]프로그래머스_달리기 경주/[Cs]프로그래머스_달리기 경주/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Programmers_RunningRace
+{
+    class mainApp
+    {
+        struct Player
+        {
+            public string name;
+            public int rank;
+        }
+
+        static public void MergeSort(ref Player[] player)
+        {
+
+
+            return;
+        }
+
+
+
+        public string[] solution(string[] players, string[] callings)
+        {
+            string[] answer = new string[players.Length];
+            Player[] player = new Player[players.Length];
+            int rank;
+
+            answer = players;
+
+            for(int i = 0; i < players.Length; i++)
+            {
+                player[i].name = players[i];
+                player[i].rank = i + 1;
+            }
+
+            MergeSort(ref player);
+
+            for (int i = 0; i < callings.Length; i++)
+            {
+                rank = SearchPlayerRank(callings[i]);
+                swap(ref answer, rank, rank - 1);
+            }
+
+            for(int i=0; i<answer.Length; i++)
+            {
+                Console.WriteLine(i + 1 + "등 : " + answer[i]);
+            }
+
+
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Compare("Apple", "Bpple"));
+
+        }
+    }
+}
